5f98925d83920aa3b59a0a9ce1bb3b23751b9d94
Nathan Graule <solarliner@gmail.com>
2021-10-17 02:48:38 +0200

    feat: util functions in lstream

--- src/utils/lstream.ml
+++ src/utils/lstream.ml
@@ -29,3 +29,39 @@ let of_list (l : 'a list) =
 
 let of_channel (i : in_channel) =
   of_stream (Stream.of_channel i)
+
+let first = function
+| Nil -> failwith "No value in lazy stream"
+| Cons (x, _) -> x
+
+let empty = function
+| Nil -> true
+| Cons _ -> false
+
+let last =
+  let rec aux acc = function
+  | Nil -> acc
+  | Cons (x, xs) -> aux x (Lazy.force xs)
+in function
+| Nil -> failwith "No value in lazy stream"
+| Cons (x, xs) -> lazy (aux x (Lazy.force xs))
+
+let rec zip a b = match a,b with
+| (Nil, _) | (_, Nil) -> Nil
+| (Cons (x,xs), Cons(y,ys)) -> Cons ((x,y), lazy (zip (Lazy.force xs) (Lazy.force ys)))
+
+let rec scan_left f i = function
+| Nil -> Nil
+| Cons (x,xs) -> let r = f i x in Cons (r, lazy (scan_left f r (Lazy.force xs)))
+
+let fold_left f i l = scan_left f i l |> last 
+
+let rec take_while pred = function
+| Nil -> Nil
+| Cons (x,xs) -> if pred x then Nil else Cons (x, lazy (take_while pred (Lazy.force xs)))
+
+let rec skip_while pred = function
+| Nil -> Nil
+| Cons (x, xs) -> if pred x
+  then skip_while pred (Lazy.force xs)
+  else Cons (x, xs)
